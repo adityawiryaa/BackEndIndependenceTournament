9ffbd05dc2b536cc858f41320eabdd137eb65156
Aditya Wirya Maulana <adityawiryamaulana@gmail.com>
2020-11-13 11:48:39 +0700

    update

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ const approute = express.Router()
 const userRoutes = require('../routes/user')
 const errorHandler = require('../helpers/errorHandlers')
 
+approute.use('/',(req,res) => {
+    res.json({mesage : 'hello'})
+})
 approute.use('/user',userRoutes)
 approute.use(errorHandler)
 
