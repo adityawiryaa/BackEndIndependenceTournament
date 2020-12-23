26d2593b8fc199b9a8a5511ddbfbc1f65acf172e
Aditya Wirya Maulana <adityawiryamaulana@gmail.com>
2020-12-23 10:28:39 +0700

    update

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController')
 const authentication = require('../middleware/authentication')
 let bouncer = require ("express-bouncer")(600000,600000,5);
 
+bouncer.whitelist.push ("127.0.0.1");
 bouncer.blocked = function (req, res, next, remaining)
 {
     res.status(429).json({status : 429, message : "Too many requests have been made, " +
@@ -12,6 +13,7 @@ bouncer.blocked = function (req, res, next, remaining)
 
 dataUser.post('/signup',userController.register)
 dataUser.post('/signin',bouncer.block,userController.login)
+bouncer.addresses = { };
 dataUser.post('/create/headman',authentication.adminAuth,userController.createHeadMan)
 dataUser.post('/create/committe',authentication.headManAuth,userController.createCommitte)
 dataUser.put('/forget/password',userController.forgetPassword)
